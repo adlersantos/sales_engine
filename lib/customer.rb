7d3ef78217cb7528a290f62ecaeedb479e69d9d0
Adler Santos <adlersantos712@gmail.com>
2014-03-05 00:40:27 +0800

    git rm'ed moved files

--- lib/customer.rb
+++
@@ -1,7 +0,0 @@
-class Customer
-  attr_reader :first_name, :last_name
-
-  def initialize(first_name, last_name)
-    @first_name, @last_name = first_name, last_name
-  end
-end
--- lib/item.rb
+++
@@ -1,7 +0,0 @@
-class Item
-  attr_reader :id, :name
-
-  def initialize(attrs)
-    attrs.each { |key, value| instance_variable_set(key, value) }
-  end
-end

ff69b5c80196a2fa592fb65acdc4a1de7c4980ed
Adler Santos <adlersantos712@gmail.com>
2014-03-04 23:33:43 +0800

    added repo attributes

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,2 +1,17 @@
 require 'csv'
 require_relative 'merchant'
+
+class SalesEngine
+  repositories = [:merchant_repository,
+                  :invoice_repository,
+                  :item_repository,
+                  :invoice_item_repository,
+                  :customer_repository,
+                  :transactions_repository]
+
+  attr_reader *repositories
+
+  def startup
+    # set all repositories as instance variables
+  end
+end
